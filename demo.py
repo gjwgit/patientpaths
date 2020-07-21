abf8ff7bf7b2ce4cca94ae559d340568e13a8286
Graham Williams <Graham.Williams@togaware.com>
2020-07-21 20:05:23 +1000

    Initial import.

---
+++ demo.py
@@ -0,0 +1,84 @@
+# -*- coding: utf-8 -*-
+#
+# Time-stamp: <Tuesday 2020-07-21 20:04:06 AEST Graham Williams>
+#
+# Copyright (c) Togaware Pty Ltd. All rights reserved.
+# Licensed under the GPLv3
+# Author: Graham.Williams@togaware.com
+#
+# ml demo patientpaths
+#
+# This demo is based on:
+#
+# https://github.com/anu-act-health-covid19-support/patientpaths
+
+from mlhub.pkg import mlask, mlcat, mlpreview
+
+mlcat("Patient Paths", """\
+Runs a model of care algorithm to identify outcomes from a configured health 
+care system. The input to the model consists of N cohorts (e.g., age groups, 
+gender, socio-economic, etc.). What the cohort is does not really matter.
+
+For each cohort the daily presentations of patients in that cohort 
+(i.e., the number of patients arriving each day to the health facility) 
+is provided. These are split into mild and severe cases.
+""")
+
+#----------------------------------------------------------------------
+# Setup
+#----------------------------------------------------------------------
+
+# Import the required libraries.
+
+import re
+
+import numpy as np
+
+from pandas       import read_excel
+from patientpaths import outcomes_for_moc
+
+fname = "cohorts4_daily36.xlsx"
+mild   = np.asarray(read_excel(fname, sheet_name="mild", header=None).T)
+severe = np.asarray(read_excel(fname, sheet_name="severe", header=None).T)
+
+risk = np.array([0, 2, 2, 0])
+
+cohorts = mild.shape[0]
+days = mild.shape[1]
+
+mlcat("", f"""\
+Thus a spreadsheet of daily presentations is loaded in this demo,
+having two tabs, one for the mild presentations and another for the
+severe presentations. Each column corresponds to a cohort and each row is
+a successive day.
+
+From the dataset we see there are {cohorts} cohorts and presentations are
+provided for {days} days (that's {cohorts*days*2} numbers).
+""")
+
+outcomes = outcomes_for_moc("cohort", mild, severe, risk)
+keys = re.sub(r", ([^,]*)$", ", and \\1", ", ".join(list(outcomes.keys())))
+
+mlask(end="\n")
+
+
+mlcat("Model of Care", f"""\
+The model of care is run to calculate the outcomes. The outcomes are reported for:
+{keys}.
+""")
+
+outcome = "deaths"
+
+mlcat("", f"""\
+The first set of outcomes reports the expected {outcome.upper()} per day per cohort.
+""")
+
+print("  " + "\t  ".join([str(i) for i in range(1, cohorts + 1)]) + "   TOTAL")
+print("-" * (cohorts*8) + "---")
+for i in range(days):
+    print("\t".join([str(a) for a in list(outcomes[outcome][i].round(decimals=1))])
+          + f"\t{round(outcomes[outcome][i].sum(), 1)}")
+print("-" * (cohorts*8) + "---")
+
+mlask(True, True)
+
